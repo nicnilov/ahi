290aaca4c3b0ce24e97ca45fd5aa997b405842e7
Nic Nilov <nicnilov@gmail.com>
2015-10-03 00:19:23 +0300

    Slug generation for blocks fixed

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -3,6 +3,7 @@ class Aircraft < ActiveRecord::Base
   friendly_id :generate_slug, use: :slugged
 
   validates_presence_of :manufacturers, :variant_type
+  validates_presence_of :priors, if: Proc.new { |aircraft| aircraft.block? }
 
   validates_uniqueness_of :name, scope: :variant_type
   validates_uniqueness_of :slug
@@ -17,25 +18,38 @@ class Aircraft < ActiveRecord::Base
   scope :families, -> { where(variant_type: :family) }
 
   # Mofifications are upgrades or developments of an aircraft, such as P-51A, P-51B
+  # A modification may inherit from multiple parents, that is have many priors, but a modifications graph
+  # within a particular family must eventually converge to a single root, which is the family itself
   # Modification name may collide with family name as is the case with P-51, see generate_slug for handling
   # Name collisions between modifications or blocks are not expected
-  scope :modifications, -> (aircraft) { where(variant_type: :modification) }
+  scope :modifications, -> (aircraft) { aircraft.variants.where(variant_type: :modification) }
 
   # Blocks are production series of a specific modification as P-51D-5-NA, P-51D-10-NA, F-16C Block 30, F-16C Block 50
+  # Blocks are not expected to refer to more than one modification
   # Blocks are not expected to refer directly to families
-  scope :blocks, -> (aircraft) { where(variant_type: :block) }
+  scope :blocks, -> (aircraft) { aircraft.variants.where(variant_type: :block) }
 
   def variant?
     self.priors.exists? # TODO: make sure variant_type is always filled out
   end
 
+  def block?
+    self.variant_type == 'block'
+  end
+
   private
 
   def generate_slug
-    [
-      [self.manufacturers.map(&:name).sort, :name, :alias].flatten,
-      [self.manufacturers.map(&:name).sort, :name, :alias, 'variant'].flatten
-    ]
+    if self.block?
+      [
+        [self.manufacturers.map(&:name).sort, self.priors.first.name, :name, :alias].flatten
+      ]
+    else
+      [
+        [self.manufacturers.map(&:name).sort, :name, :alias].flatten,
+        [self.manufacturers.map(&:name).sort, :name, :alias, 'variant'].flatten
+      ]
+    end
   end
 
   def should_generate_new_friendly_id?
